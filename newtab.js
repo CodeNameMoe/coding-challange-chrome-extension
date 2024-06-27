a35434fc085401b176c31de55eeb5ac3ff183202
codenamemoe <newmoeali@gmail.com>
2024-06-27 12:30:49 +0100

    tab random colour change

---
+++ newtab.js
@@ -0,0 +1,13 @@
+// Function to get a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  // Change the background color of the body
+  document.body.style.backgroundColor = getRandomColor();
+  
